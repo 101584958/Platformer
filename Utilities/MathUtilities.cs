fe880bba6ac38f4fddc8dc7ddd12bd286a0b6a82
102113528 <102113528@student.swin.edu.au>
2019-10-06 03:04:23 +1100

    Add basic vector and math utility classes

---
+++ Utilities/MathUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Template.Utilities
+{
+    public class MathUtilities
+    {
+        private static Random _random = new Random();
+
+        public static int Random(int minimum, int maximum)
+        {
+            return _random.Next(minimum, maximum);
+        }
+
+        public static T Clamp<T>(T value, T minimum, T maximum) where T : IComparable<T>
+        {
+            return value.CompareTo(minimum) < 0 ? minimum : value.CompareTo(maximum) > 0 ? maximum : value;
+        }
+    }
+}
