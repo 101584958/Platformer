b88c7b4c46bc765e84e585ee291626b8eae28b18
101696057 <rajnegi266@gmail.com>
2019-10-06 13:39:30 +1100

    implement player

---
+++ Entities/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwinGameSDK;
+
+namespace Template.Entities
+{
+    class Player : Actor
+    {
+        public Player(Bitmap bitmap) : base(bitmap)
+        {
+        }
+
+        public override void OnUpdate(EntityManager entityManager)
+        {
+            if (SwinGame.KeyTyped(KeyCode.vk_r))
+            {
+                Position.Y -= 32;
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_RIGHT))
+            {
+                Velocity.X += 12;
+            }
+            
+           if (SwinGame.KeyDown(KeyCode.vk_LEFT))
+            {
+                Velocity.X -= 12;
+            }
+           
+               if (SwinGame.KeyDown(KeyCode.vk_UP)&& OnGround)
+            {
+                Velocity.Y -= 32;
+            }
+
+
+            base.OnUpdate(entityManager);
+        }
+    }
+}
